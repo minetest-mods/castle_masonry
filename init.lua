94b25b549b502ceeff8d5aa596ddc977745f43ed
FaceDeer <derksenmobile@gmail.com>
2017-02-28 23:29:44 -0700

    fix an alias

--- init.lua
+++ init.lua
@@ -112,7 +112,7 @@ minetest.register_alias("castle:pillars_top", "castle_masonry:pillars_stonewall_
 minetest.register_alias("castle:pillars_middle", "castle_masonry:pillars_stonewall_middle")
 minetest.register_alias("castle:arrowslit", "castle_masonry:arrowslit_stonewall")
 minetest.register_alias("castle:arrowslit_hole", "castle_masonry:arrowslit_stonewall_hole")
-minetest.register_alias("castle:arrowslit", "castle_masonry:arrowslit_stonewall_cross")
+minetest.register_alias("castle:arrowslit_cross", "castle_masonry:arrowslit_stonewall_cross")
 
 for _, material in pairs(castle_masonry.materials) do
 	castle_masonry.register_murderhole_alias("castle", material.name, "castle_masonry", material.name)
